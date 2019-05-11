076af85c8231c48a8a14102f64f739d7df4d82ac
Kevin Isom <kevin.isom@gmail.com>
2019-05-11 20:00:38 +1200

    Styles and data

--- src/ui/features/home.js
+++ src/ui/features/home.js
@@ -1,2 +1,23 @@
-import React from "react"
-export default () => <main><h1>My App</h1></main>
+import React from 'react'
+import { useGet } from '@brightleaf/react-hooks'
+import Loading from '../components/loading'
+import ProductCard from '../components/product-card'
+import styles from './home.css'
+
+export default () => {
+  const { data, error, loading } = useGet(
+    'https://kev-pi.herokuapp.com/api/products'
+  )
+  if (error) {
+    return <div>There was an error</div>
+  }
+  console.info('data', data)
+  const prods = data.map(p => <ProductCard key={`product-${p.id}`} {...p} />)
+  return (
+    <main>
+      <h2>Products</h2>
+      {loading && <Loading />}
+      <div className={styles.cards}>{prods}</div>
+    </main>
+  )
+}
