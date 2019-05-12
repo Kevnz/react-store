a7bd7805eade8c2506591ca12d177f6757d34802
Kevin Isom <kevin.isom@gmail.com>
2019-05-12 21:51:47 +1200

    use context and reducer

--- src/ui/components/product-card/product-card.js
+++ src/ui/components/product-card/product-card.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCartPlus } from '@fortawesome/free-solid-svg-icons'
+import { CartContext } from '../../core/container'
 import styles from './product-card.css'
 const ProductCard = ({
   id,
@@ -11,13 +12,23 @@ const ProductCard = ({
   image,
   details,
 }) => {
-  console.log('styles', styles)
+  const { dispatch, ...state } = useContext(CartContext)
+  console.log('cart state', state)
   return (
     <div className={styles.card}>
       <img className={styles.product} src={image} alt={name} />
       <h3>{name}</h3>
       <p>{description}</p>
-      <a href="#">
+      <a
+        href="#"
+        onClick={e => {
+          e.preventDefault()
+          dispatch({
+            type: 'cart.add',
+            payload: { product: { id, name, price } },
+          })
+        }}
+      >
         {price}
         <FontAwesomeIcon icon={faCartPlus} />
       </a>
