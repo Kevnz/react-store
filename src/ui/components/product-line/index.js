008785d331b350d238faf7e4f8a80cbb2e580680
Kevin Isom <kevin.isom@gmail.com>
2019-05-13 16:32:01 +1200

    Product line item component

---
+++ src/ui/components/product-line/index.js
@@ -0,0 +1,4 @@
+import ProductLine from './product-line'
+
+export { ProductLine }
+export default ProductLine
---
+++ src/ui/components/product-line/product-line.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMinusSquare } from '@fortawesome/free-solid-svg-icons'
+
+import styles from './product-line.css'
+const ProductLine = ({
+  id,
+  name,
+  price,
+  quantity,
+  category,
+  description,
+  image,
+  details,
+  removeFromCart,
+}) => {
+  return (
+    <div className={styles.lineItem}>
+      <p>
+        <a
+          href="#"
+          onClick={e => {
+            e.preventDefault()
+            removeFromCart()
+          }}
+        >
+          <img className={styles.product} src={image} />
+          {name} - {quantity} : {quantity * price}
+          <FontAwesomeIcon icon={faMinusSquare} />
+        </a>
+      </p>
+    </div>
+  )
+}
+
+export default ProductLine
