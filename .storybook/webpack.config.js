d2a27735d45ae345ecfc9f964cbbce299c27f38d
Kevin Isom <kevin.isom@gmail.com>
2019-05-13 16:23:54 +1200

    Storybook config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,18 +1,54 @@
-// you can use this file to add your custom webpack plugins, loaders and anything you like.
-// This is just the basic way to add additional webpack configurations.
-// For more information refer the docs: https://storybook.js.org/configurations/custom-webpack-config
+const webpack = require('webpack')
 
-// IMPORTANT
-// When you add this file, we won't add the default configurations which is similar
-// to "React Create App". This only has babel loader to load JavaScript.
+const path = require('path')
 
-module.exports = {
-  plugins: [
-    // your custom plugins
-  ],
+
+const baseConfig = {
+  entry: './src/ui/index.js',
   module: {
     rules: [
-      // add your custom rules.
+      {
+        test: /\.(mjs|js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+
+      {
+        test: /\.css$/,
+        use: [
+           'style-loader',
+          'css-loader?modules',
+        ],
+      },
+      {
+        test: /\.s[ca]ss$/,
+        loader: 'style-loader!css-loader!resolve-url-loader!sass-loader',
+      },
     ],
   },
-};
+  resolve: {
+    extensions: ['*', '.mjs', '.js', '.jsx'],
+    modules: ['node_modules', 'src'],
+    alias: {
+
+    },
+  },
+}
+
+const devConfig = {
+
+  plugins: [
+    new webpack.DefinePlugin({
+      process: {
+        env: {
+          NODE_ENV: '"development"',
+        },
+      },
+    }),
+  ],
+}
+
+module.exports = {
+  ...baseConfig,
+  ...devConfig,
+}
