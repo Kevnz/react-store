9790b8de263765fd85961107636a8ff2aa9a7b54
Kevin Isom <kevin.isom@gmail.com>
2019-05-15 07:27:19 +1200

    Add "purchase" interaction

--- src/ui/features/cart/cart.js
+++ src/ui/features/cart/cart.js
@@ -1,13 +1,17 @@
-import React, { useContext } from 'react'
-import { Button } from 'react-form-elements'
+import React, { useContext, useRef } from 'react'
+import { usePost } from '@brightleaf/react-hooks'
+
 import { CartContext } from '../../core/container'
 import Loading from '../../components/loading'
 import ProductLineItem from '../../components/product-line'
+import Modal from '../../components/modal'
+import CreditCardForm from '../../components/creditcard-form'
 
 export function Cart() {
   const { items, totalCost, dispatch, ...state } = useContext(CartContext)
-  console.log('state', state)
-  console.log('items', items)
+  const { data, error, loading, postData } = usePost(
+    'https://kev-pi.herokuapp.com/api/long/wait'
+  )
   const prods = items.map(product => (
     <ProductLineItem
       key={`product-${product.id}`}
@@ -20,11 +24,34 @@ export function Cart() {
       }}
     />
   ))
+  const modalRef = useRef()
   return (
     <div>
       <div className="checkout">{prods}</div>
       <div>Total Cost: ${totalCost}</div>
-      <Button className="primary-btn">Purchase</Button>
+      <Modal
+        buttonText="Purchase"
+        buttonClass="primary-btn"
+        show={false}
+        ref={modalRef}
+      >
+        <CreditCardForm
+          onSubmit={async values => {
+            if (loading) return
+
+            const { result } = await postData(values)
+            if (result) {
+              dispatch({
+                type: 'cart.complete',
+              })
+              modalRef.current.hide()
+            }
+          }}
+          onCancel={e => {
+            modalRef.current.hide()
+          }}
+        />
+      </Modal>
     </div>
   )
 }
