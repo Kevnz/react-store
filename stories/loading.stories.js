f429bb56a58de61d0f90eba1778378dbab6a8a49
Kevin Isom <kevin.isom@gmail.com>
2019-05-15 22:10:28 +1200

    loading stories

--- stories/loading.stories.js
+++ stories/loading.stories.js
@@ -8,5 +8,12 @@ import Loading from '../src/ui/components/loading'
 
 storiesOf('Loading', module)
   .add('The Loading Indicator', () => <Loading />)
+  .add('The Loading Indicator Extra Large', () => (
+    <Loading size="extra-large" />
+  ))
+  .add('The Loading Indicator Large', () => <Loading size="large" />)
   .add('The Loading Indicator Medium', () => <Loading size="medium" />)
   .add('The Loading Indicator Small', () => <Loading size="small" />)
+  .add('The Loading Indicator Extra Small', () => (
+    <Loading size="extra-small" />
+  ))
