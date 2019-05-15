a9a52050eb862cc86f106a7ce27b8f37350cf77b
Kevin Isom <kevin.isom@gmail.com>
2019-05-15 17:06:38 +1200

    use root level export

--- stories/loading-button.stories.js
+++ stories/loading-button.stories.js
@@ -4,7 +4,7 @@ import { storiesOf } from '@storybook/react'
 
 import { action } from '@storybook/addon-actions'
 
-import LoadingButton from '../src/ui/components/loading/button'
+import { LoadingButton } from '../src/ui/components/loading'
 
 const LoadingBounce = () => {
   const [loading, setLoading] = useState(false)
