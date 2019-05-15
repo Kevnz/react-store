e167b5c8881c8dab75d310f8aebcddec05747406
Kevin Isom <kevin.isom@gmail.com>
2019-05-15 22:06:34 +1200

    Ink in the button

--- src/ui/components/button/button.js
+++ src/ui/components/button/button.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import { Button } from 'react-form-elements'
+import Ink from 'react-ink'
 import classnames from 'classnames'
+
 import styles from './button.css'
 export default function PrimaryButton({
   className,
   variation = 'default',
+  children,
   ...props
 }) {
   return (
-    <Button {...props} className={classnames(styles[variation], className)} />
+    <Button {...props} className={classnames(styles[variation], className)}>
+      {children}
+      <Ink />
+    </Button>
   )
 }
